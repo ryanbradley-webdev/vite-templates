078876a52c2f343f6d87be8750a7a65db298b793
Ryan Bradley <ryanbradley315@gmail.com>
2023-10-13 20:35:32 -0500

    Install react-router and react-query libraries. Update main.tsx to utilize both libraries. Make new routes directory with Home route. Add type declaration file.

--- react-ts-router-query/src/main.tsx
+++ react-ts-router-query/src/main.tsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { BrowserRouter } from 'react-router-dom'
 import './index.css'
+
+const client = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={client}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </QueryClientProvider>
   </React.StrictMode>,
 )
---
+++ react-ts-router-query/src/routes/Home/Home.tsx
@@ -0,0 +1,19 @@
+import styles from './Home.module.css'
+
+export default function Home() {
+    return (
+        <main
+            className={styles.main}
+        >
+
+            <h1>
+                Vite Template
+            </h1>
+
+            <h2>
+                TypeScript, React-Router, Tanstack Query
+            </h2>
+
+        </main>
+    )
+}
