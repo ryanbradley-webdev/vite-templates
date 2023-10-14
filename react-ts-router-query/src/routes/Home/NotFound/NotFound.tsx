c87f8c40672e588084b73e63f680c9454d24f72b
Ryan Bradley <ryanbradley315@gmail.com>
2023-10-13 21:03:46 -0500

    Add 404 route.

---
+++ react-ts-router-query/src/routes/Home/NotFound/NotFound.tsx
@@ -0,0 +1,15 @@
+import styles from './NotFound.module.css'
+
+export default function NotFound() {
+    return (
+        <main
+            className={styles.main}
+        >
+
+            <h1>
+                The page you&apos;re looking for can&apos;t be found.
+            </h1>
+
+        </main>
+    )
+}
