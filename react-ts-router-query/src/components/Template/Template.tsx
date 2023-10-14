3529d090590b21cb519feb3f053dd6c7390614a9
Ryan Bradley <ryanbradley315@gmail.com>
2023-10-13 20:31:15 -0500

    Replace Vite boilerplate with template components.

---
+++ react-ts-router-query/src/components/Template/Template.tsx
@@ -0,0 +1,13 @@
+import styles from './Template.module.css'
+
+export default function Template() {
+    return (
+        <div
+            className={styles.template}
+        >
+
+            {/* Component content */}
+
+        </div>
+    )
+}
